a34173c6d63aad022abfd271c47621ac594d0890
Claudia Vargas <clva4454@colorado.edu>
2026-02-20 22:50:32 -0700

    Date

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
-import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
 const metadataDefinition = () =>
   z
@@ -49,8 +49,8 @@ const metadataDefinition = () =>
 const postCollection = defineCollection({
   loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/data/post' }),
   schema: z.object({
-    publishDate: z.date().optional(),
-    updateDate: z.date().optional(),
+    publishDate: z.coerce.date().optional(),
+    updateDate: z.coerce.date().optional(),
     draft: z.boolean().optional(),
 
     title: z.string(),
